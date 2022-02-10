64f9a131669941089268d56f891f43da8ee36644
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-02-09 19:37:43 -0700

    added style and index.html

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const db = require('./db');
+const path = require("path"); 
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-const db = require('./db');
 
+app.get('/',(req,res) => { 
+  res.sendFile(path.join(__dirname+'/index.html')); 
+}); 
 app.get('/getNames', db.getNames);
 app.get('/getContacts', db.getContacts);
 app.get('/updateContactFirstNameByIds/id/:id/firstname/:firstname', db.updateContactFirstNameById);
 app.get('/updateContactById/id/:id', db.updateContactById);
 
-app.get('/', (req, res)=>{
-    res.send('hello');
-})
+// app.get('/', (req, res)=>{
+//     res.send('hello');
+// })
 
 app.listen(port, ()=>{
     console.log(`server is up on port ${port}`);
